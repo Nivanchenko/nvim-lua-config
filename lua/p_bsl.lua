549ff7621e4156ad19354a36f959ab1df6f58489
Ivanchenko Nikita <n.ivanchenko@tcsbank.ru>
2021-11-17 14:32:56 +0300

    nativt lsp bsl support

--- lua/p_bsl.lua
+++ lua/p_bsl.lua
@@ -1,12 +1,10 @@
 
 local packer = require('packer')
-packer.use({ 'Nivanchenko/bsl-lsp.nvim' })
 packer.use({ 'andreevlex/vim-language-1c-bsl' })
 packer.use({ '1c-syntax/vsc-language-1c-bsl' })
 
 pcall(function()
-    -- Непосредственно подключение к bsl lsp
-    require('bsl-lsp')
+    local util = require 'lspconfig/util'
 
     -- Любым удобным способом указываем путь к бинарю
     local bslbinary = vim.fn.stdpath('data') .. "/bsl-language-server/bin/bsl-language-server"
@@ -14,7 +12,10 @@ pcall(function()
     local caps = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     -- Подключаем модуль lsp конфига, и запускаем анализатор bsl
-    require'lspconfig'.bsllsp.setup {
+    require'lspconfig'.bsl_ls.setup {
+        root_dir = function(fname)
+                       return util.find_git_ancestor(fname) or util.path.dirname(fname)
+                   end,
         -- Определяем команду, которой запускается lsp 
         capabilities = caps,
         cmd = {bslbinary, "lsp"}

fe49f16e001ce5a56ee9c70821743cd25df8c21b
Никита Иванченко <nikita.ivanchenko@vkteam.ru>
2025-06-08 23:43:00 +0300

    rem rust, update mason

--- lua/plugins/lspsupport.lua
+++ lua/plugins/lspsupport.lua
@@ -5,28 +5,12 @@ return {
         require("mason").setup()
     end,
     },
-    {"williamboman/mason-lspconfig.nvim",
-        config = function()
-        
-        require("mason-lspconfig").setup { ensure_installed = { "bsl_ls", "rust_analyzer" } }
-
-        require("mason-lspconfig").setup_handlers {
-                function (server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {}
-                end,
-                ["bsl_ls"] = function ()
-                    require'lspconfig'.bsl_ls.setup {
-                        root_dir = function(fname)
-                                       local util = require 'lspconfig/util'
-                                       return util.find_git_ancestor(fname) or util.path.dirname(fname)
-                                   end,
-                        cmd = {vim.fn.stdpath('data') .. "/mason/bin/bsl-language-server", "lsp"}
-                    }
-                end
-        }
-                end,
+    {"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = { "lua_ls", "rust_analyzer" },
+	    }
     },
-  "neovim/nvim-lspconfig",
+"neovim/nvim-lspconfig",
   {'nvimdev/lspsaga.nvim',
     config = function()
         require('lspsaga').setup({
